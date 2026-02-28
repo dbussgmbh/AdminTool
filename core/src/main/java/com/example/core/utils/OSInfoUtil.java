6e7e2ae605f24aebbfd9db199a7a7b3f22b25b2c
Michael Quaschny <m.quaschny@dbuss.de>
2026-02-28 17:58:03 +0100

    Initial

---
+++ core/src/main/java/com/example/core/utils/OSInfoUtil.java
@@ -0,0 +1,34 @@
+package com.example.core.utils;
+
+import java.io.Serializable;
+
+
+public final class OSInfoUtil implements Serializable {
+	
+	private static final long serialVersionUID = 8418286295150252695L;
+	
+	public static final String OS_NAME = System.getProperty("os.name");
+	public static final String OS_TYPE = System.getProperty("os.arch");;
+	public static final String OS_VERSION = System.getProperty("os.version");
+
+	public static String getOsName() {
+		return OS_NAME;
+	}
+	public static String getOsType() {
+		return OS_TYPE;
+	}
+	public static String getOsVersion() {
+		return OS_VERSION;
+	}
+	
+	public String getOsIcon() {
+		if (OS_NAME.toLowerCase().contains("windows"))
+			return "windows.png";
+		else if (OS_NAME.toLowerCase().contains("mac"))
+			return "apple.png";
+		else if (OS_NAME.toLowerCase().contains("linux"))
+			return "linux.ico";
+		else 
+			return "unknown.png";
+	}
+}
