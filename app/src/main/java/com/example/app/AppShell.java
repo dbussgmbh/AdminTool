d5a4010bfb22ee6381653d597859c8b6eb465a7c
Michael <m.quaschny@dbuss.de>
2026-03-05 21:14:47 +0100

    Classes hinzu

--- app/src/main/java/com/example/app/AppShell.java
+++ app/src/main/java/com/example/app/AppShell.java
@@ -1,8 +1,15 @@
 package com.example.app;
 
+import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.theme.Theme;
 
+@Uses(Tab.class)
+@Uses(Tabs.class)
+@Uses(RadioButtonGroup.class)
 @Theme("admintheme")
 public class AppShell implements AppShellConfigurator {
 }
